90108c45c1100289e042c74a56639853c6abf9cc
myue123 <59975304+myue123@users.noreply.github.com>
2022-12-05 16:26:26 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "Hello $INPUT_MYINPUT"
 memory=$(cat /proc/meminfo)
-echo "{memory}=$memory >> $GITHUB_OUTPUT "
+echo "memory=$memory >> $GITHUB_OUTPUT "
 #echo "::set-output name=memory::$memory"
